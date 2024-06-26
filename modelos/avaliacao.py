8470611fb770cb9965f173fc131f0a886e392d0d
annaalucia <162130074+annaalucia@users.noreply.github.com>
2024-06-26 08:14:26 -0300

    Delete modelos directory

--- modelos/avaliacao.py
+++
@@ -1,7 +0,0 @@
-class Avaliacao:
-    def __init__(self, nota, comentario):
-        self.nota = nota
-        self.comentario = comentario
-
-    def __str__(self):
-        return f"Avaliação: {self.nota} - Comentário: {self.comentario}"
